2d67e771f8a76921fdaa8385002ff7917a283390
“vinaykp” <vinaya.krishna.dev@gmail.com>
2018-07-22 13:03:33 +0530

    readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # portfolio-project
 ## A Portfolio page for my personal use
+
+- It is hosted in [https://vinaya-krishna.github.io/index](https://vinaya-krishna.github.io/index)
+
+- I have provided a login page from where admin can login to view the messages sent to him from the index page.[https://vinaya-krishna.github.io/login](https://vinaya-krishna.github.io/login)
+
+Here I am using a already existing back-end server.
+- On Clicking Send Message in [Index Page](https://vinaya-krishna.github.io/index)(Portfolio), Message is sent to server(Currently I have disabled this feature). 
+- Received messages can be viewed by loging into the [login page](https://vinaya-krishna.github.io/login), Which will redirect into [Messages](https://vinaya-krishna.github.io/messages) page.
+
+> Login Credentials
+> {"email":"puranik@digdeeper.in","password":"test123"}
+
+
+There are some issues , I will be resolving it whenever I get time.
+- [x] Fix Reply in Messages page
+- [ ] If user tries to open Messages page directly redirect to login page.
+- [ ] Build my own backend server and host the app in heroku.
--- js/portfolio.js
+++ js/portfolio.js
@@ -5,56 +5,68 @@ const message = document.getElementById('message');
 const resetButton = document.getElementById('reset');
 const contactForm = document.getElementById('contact-form');
 
+let messageSend = false;
+
 contactForm.addEventListener('submit', function(event){
     event.preventDefault();
-    if(name.value == ""){
-        name.classList.add('is-invalid');
-        $('#name').tooltip('show');
-        setTimeout(() => {
-            name.classList.remove('is-invalid');
-            $('#name').tooltip('hide');
-        }, 2000);
+    if(messageSend){
+        if(name.value == ""){
+            name.classList.add('is-invalid');
+            $('#name').tooltip('show');
+            setTimeout(() => {
+                name.classList.remove('is-invalid');
+                $('#name').tooltip('hide');
+            }, 2000);
+        }
+    
+        if(email.value == ""){
+            email.classList.add('is-invalid');
+            $('#email').tooltip('show');
+            setTimeout(() => {
+                email.classList.remove('is-invalid');
+                $('#email').tooltip('hide');
+            }, 2000);
+        }
+        
+        if(message.value == ""){
+            message.classList.add('is-invalid');
+            $('#message').tooltip('show');
+            setTimeout(() => {
+                message.classList.remove('is-invalid');
+                $('#message').tooltip('hide');
+            }, 2000);
+        }
+        if( name.value != "" && email.value != "" && message.value != ""){
+            const userData = {
+                name : name.value,
+                email: email.value,
+                phone: phone.value,
+                message: message.value
+            }
+            console.log(userData);
+            var url = 'https://personal-portfolio-page.herokuapp.com/messages';
+            var data = userData;
+    
+            fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+            }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(response => console.log('Success:', response));
+        }
     }
-
-    if(email.value == ""){
-        email.classList.add('is-invalid');
-        $('#email').tooltip('show');
-        setTimeout(() => {
-            email.classList.remove('is-invalid');
-            $('#email').tooltip('hide');
-        }, 2000);
+    else{
+        console.log("Alert");
+        $("#feedback").css({'display': 'block'});
+        setTimeout(()=>{
+            $("#feedback").css({'display': 'none'});
+        },5000)
+        
     }
     
-    if(message.value == ""){
-        message.classList.add('is-invalid');
-        $('#message').tooltip('show');
-        setTimeout(() => {
-            message.classList.remove('is-invalid');
-            $('#message').tooltip('hide');
-        }, 2000);
-    }
-    if( name.value != "" && email.value != "" && message.value != ""){
-        const userData = {
-            name : name.value,
-            email: email.value,
-            phone: phone.value,
-            message: message.value
-        }
-        console.log(userData);
-        var url = 'https://personal-portfolio-page.herokuapp.com/messages';
-        var data = userData;
-
-        fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-        }).then(res => res.json())
-        .catch(error => console.error('Error:', error))
-        .then(response => console.log('Success:', response));
-    }
-
 });
 
 
